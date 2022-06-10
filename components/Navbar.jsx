0526f518115a04c7360c9129ecff7b996a9253af
Ted McGinness <ttmcginness22@gmail.com>
2022-06-10 14:15:17 -0500

    mobile menu added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -68,6 +68,7 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme();
   const router = useRouter();
   const [active, setActive] = useState('Explore NFTs');
+  const [isOpen, setisOpen] = useState(false);
 
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
@@ -101,6 +102,43 @@ const Navbar = () => {
         </div>
       </div>
 
+      <div className="hidden md:flex ml-2">
+        {isOpen ? (
+          <Image
+            src={images.cross}
+            objectFit="contain"
+            width={25}
+            height={25}
+            alt="close"
+            onClick={() => setisOpen(false)}
+            className={theme === 'light' && 'filter invert'}
+
+          />
+        )
+          : (
+            <Image
+              src={images.menu}
+              objectFit="contain"
+              width={25}
+              height={25}
+              alt="menu"
+              onClick={() => setisOpen(true)}
+              className={theme === 'light' && 'filter invert'}
+            />
+          )}
+
+        {isOpen && (
+        <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+          <div className="flex-1 p-4">
+            <MenuItems active={active} setActive={setActive} isMobile />
+          </div>
+          <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
+            <ButtonGroup setActive={setActive} router={router} />
+          </div>
+        </div>
+        )}
+
+      </div>
     </nav>
   );
 };
