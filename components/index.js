8ba7aa5d8ae4b81e90b466ba12c7da7f326a1715
Ted McGinness <ttmcginness22@gmail.com>
2022-07-06 11:44:06 -0500

    loader created

--- components/index.js
+++ components/index.js
@@ -5,5 +5,6 @@ import Banner from './Banner';
 import NFTCard from './NFTCard';
 import CreatorCard from './CreatorCard';
 import Input from './Input';
+import Loader from './Loader';
 
-export { Navbar, Input, NFTCard, CreatorCard, Button, Banner, Footer };
+export { Navbar, Input, Loader, NFTCard, CreatorCard, Button, Banner, Footer };
