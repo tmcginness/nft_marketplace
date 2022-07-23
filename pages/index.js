9cc206d6e20e013f201652ab2edeabc07dd8bcfe
Ted McGinness <ttmcginness22@gmail.com>
2022-07-23 16:02:33 -0500

    top creators section is now live with live data

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,9 @@ import { useTheme } from 'next-themes';
 import { Banner, CreatorCard, NFTCard } from '../components';
 import images from '../assets';
 import { makeId } from './utils/makeId';
-
+import { getCreators } from './utils/getTopCreators';
 import { NFTContext } from '../context/NFTContext';
+import { shortenAddress } from './utils/shortenAddress';
 
 const Home = () => {
   const { fetchNFTs } = useContext(NFTContext);
@@ -54,6 +55,8 @@ const Home = () => {
     };
   });
 
+  const topCreators = getCreators(nfts);
+
   return (
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
@@ -69,13 +72,13 @@ const Home = () => {
             ref={parentRef}
           >
             <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
-              {[6, 7, 8, 9, 10].map((i) => (
+              {topCreators.map((creator, i) => (
                 <CreatorCard
-                  key={`creator-${i}`}
-                  rank={i}
-                  creatorImage={images[`creator${i}`]}
-                  creatorName={`0x${makeId(3)}...${makeId(4)}`}
-                  creatorEths={10 - i * 0.5}
+                  key={creator.seller}
+                  rank={i + 1}
+                  creatorImage={images[`creator${i + 1}`]}
+                  creatorName={shortenAddress(creator.seller)}
+                  creatorEths={creator.sum}
                 />
               ))}
               {!hideButtons && (
---
+++ pages/utils/getTopCreators.js
@@ -0,0 +1,16 @@
+export const getCreators = (nfts) => {
+  const creators = nfts.reduce((creatorObject, nft) => {
+    (creatorObject[nft.seller] = creatorObject[nft.seller] || []).push(nft);
+
+    return creatorObject;
+  }, {});
+
+  Object.entries(creators);
+
+  return Object.entries(creators).map((creator) => {
+    const seller = creator[0];
+    const sum = creator[1].map((item) => Number(item.price)).reduce((prev, curr) => prev + curr, 0);
+
+    return ({ seller, sum });
+  });
+};
